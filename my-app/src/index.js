131e6b79239f4b8b86b107d271a8f4bf162ad462
cesarucal <cesar.garciar1@ucalgary.ca>
2024-03-06 15:00:23 -0700

    changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
-import React from 'areact';
-import ReactDOM from 'areact-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
