11cb706dac6466e58acf7158c183c565dcb64e64
WassimBelghache <wassim.belghache@ucalgary.ca>
2024-03-06 17:22:24 -0700

    finished 3.6

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -52,13 +52,14 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
@@ -95,19 +96,27 @@ class Game extends React.Component {
     );
 }
 
-  handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+handleClick(i) {
+  const history = this.state.history.slice(0, this.state.stepNumber + 1);
+  const current = history[history.length - 1];
+  const squares = current.squares.slice();
+  if (calculateWinner(squares) || squares[i]) {
+    return;
+  }
+  squares[i] = this.state.xIsNext ? 'X' : 'O';
+  this.setState({
+    history: history.concat([{
+      squares: squares
+    }]),
+    stepNumber: history.length,
+    xIsNext: !this.state.xIsNext,
+  });
+}
+
+  jumpTo(step) {
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
     });
   }
 }
